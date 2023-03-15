f767f871d44d190e51e003dcb02c528699345d00
Demilade Sodimu <demilade.sodimu@pwc.com>
2023-03-15 14:10:08 +0100

    .

---
+++ UploadDocumentLearn/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using SuperHeroAPI.Repository.Implementation;
+using UploadDocumentLearn.Models.Domain;
+using UploadDocumentLearn.Repository.Abstract;
+using UploadDocumentLearn.Repository.Implementation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "Uploads")),
+    RequestPath = "/Resources"
+}); //this maps the Upload folder to the resource folder
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
